cb79e27ba44b287378390aecf0eb8f1ec8333b57
Pham Tu <tubackkhoa@gmail.com>
2025-06-18 10:08:16 +0700

    change title to thesis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Metadata } from 'next';
 import { Suspense } from 'react';
 
 export const metadata: Metadata = {
-  title: 'Chat - Perplexica',
-  description: 'Chat with the internet, chat with Perplexica.',
+  title: 'Chat - Thesis',
+  description: 'Chat with the internet, chat with Thesis.',
 };
 
 const Home = () => {
--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -53,7 +53,7 @@ const Chat = ({
     };
 
     if (messages.length === 1) {
-      document.title = `${messages[0].content.substring(0, 30)} - Perplexica`;
+      document.title = `${messages[0].content.substring(0, 30)} - Thesis`;
     }
 
     if (messages[messages.length - 1]?.role == 'user') {
